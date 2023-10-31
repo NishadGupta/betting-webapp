9136729ddb4df3baa615f0e0e759d4003c97ed0b
Shreyas Sawant <shreyassawant018@gmail.com>
2023-10-31 12:28:13 -0400

    added skipped-game listener

--- betting-game-app/src/components/PlaceBet.jsx
+++ betting-game-app/src/components/PlaceBet.jsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from "react";
-import {TextField, Button} from "@mui/material";
+import { TextField, Button } from "@mui/material";
 import { socket } from "../socket";
 
-export const PlaceBet = ({userData, joinedRoom}) =>{
+export const PlaceBet = ({ userData, joinedRoom }) => {
     const [bet, setBet] = useState(-1);
     const [continueGame, setContinueGame] = useState(false);
+    const [skippedGame, setSkippedGame] = useState(false);
     const [lostGame, setLostGame] = useState(false);
     const [wonGame, setWonGame] = useState(false);
     const [userWinnings, setUserWinnings] = useState(null);
-    
-    const handleBetValue = (event)=>{
+
+    const handleBetValue = (event) => {
         setBet(event.target.value);
         setContinueGame(true);
     }
@@ -19,74 +20,92 @@ export const PlaceBet = ({userData, joinedRoom}) =>{
     const handlePlaceBet = () => {
         const playerBetDeets = {
             'game_uuid': joinedRoom.game_uuid,
-            'uuid':userData.uuid,
-            'bet':parseInt(bet),
+            'uuid': userData.uuid,
+            'bet': parseInt(bet),
         }
-        socket.emit('place_bets',playerBetDeets);
+        socket.emit('place_bets', playerBetDeets);
         setBetValue(0)
     }
-    useEffect(()=>{
-        function onContinue(value){
+    useEffect(() => {
+        function onContinue(value) {
             console.log(`RoundContinued for ${userData.uuid}`);
             console.log(value);
             setContinueGame(true);
             setLostGame(false);
             setWonGame(false);
+            setSkippedGame(false);
         }
-        function onWinner(value){
+        function onWinner(value) {
             console.log(`Round won by ${userData.uuid}`);
             console.log(value);
             setContinueGame(false);
             setLostGame(false);
             setWonGame(true);
+            setSkippedGame(false);
             setUserWinnings(value);
         }
-        function onLost(value){
+        function onLost(value) {
             console.log(`Round lost by ${userData.uuid}`);
             console.log(value);
             setContinueGame(false);
             setLostGame(true);
             setWonGame(false);
+            setSkippedGame(false);
         }
-        socket.on('continue-game',onContinue);
-        socket.on('winner',onWinner);
-        socket.on('lost',onLost);
-        return ()=>{
+        function onSkipped(value) {
+            console.log(`Round skipped by ${userData.uuid}`);
+            console.log(value);
+            setContinueGame(false);
+            setLostGame(false);
+            setSkippedGame(true);
+            setWonGame(false);
+        }
+        socket.on('continue-game', onContinue);
+        socket.on('winner', onWinner);
+        socket.on('lost', onLost);
+        socket.on('skipping-round', onSkipped);
+        return () => {
             socket.off('continue-game', onContinue);
             socket.off('winner', onWinner);
             socket.off('lost', onLost);
+            socket.off('skipping-round', onSkipped);
         };
-    },[continueGame, lostGame, wonGame])
-    return(<>
-    {continueGame ?
-        <div>
-            <h1>You've joined Room: {joinedRoom.game_uuid}</h1>
-            <TextField
-                placeholder='Place Bet value'
-                value={bet}
-                name='betValue'
-                onChange={handleBetValue}
-            />
-            <Button type="submit" onClick={handlePlaceBet}>Submit</Button>
-        </div>
-    : lostGame ?
-        <div>
-            <h1>Sorry You've lost the game, better luck next time</h1>
-        </div>
-    : wonGame ?
-        <div>
-            <h1>Congratulations you've won the game!</h1>
-        </div>
-    : <div>
-        <h1>Continue: {joinedRoom.game_uuid}</h1>
-        <TextField
-            placeholder='Place Bet value'
-            value={bet}
-            name='betValue'
-            onChange={handleBetValue}
-        />
-        <Button type="submit" onClick={handlePlaceBet}>Submit</Button>
-    </div>
-    }
+    }, [continueGame, lostGame, wonGame, skippedGame])
+    return (<>
+        {continueGame ?
+            <div>
+                <h1>Continue: {joinedRoom.game_uuid}</h1>
+                <TextField
+                    placeholder='Place Bet value'
+                    value={bet}
+                    name='betValue'
+                    onChange={handleBetValue}
+                />
+                <Button type="submit" onClick={handlePlaceBet}>Submit</Button>
+            </div>
+            : lostGame ?
+                <div>
+                    <h1>Sorry You've lost the game, better luck next time</h1>
+                </div>
+                : wonGame ?
+                    <div>
+                        <h1>Congratulations you've won the game! Here's your price </h1>
+                    </div>
+                    : skippedGame ?
+                        <div>
+                            <h1>Congratulations you've won this round and skipped the next round!</h1>
+                        </div>
+                        :
+                        <div>
+                            <h1>You've joined Room: {joinedRoom.game_uuid}</h1>
+                            <TextField
+                                placeholder='Place Bet value'
+                                value={bet}
+                                name='betValue'
+                                onChange={handleBetValue}
+                            />
+                            <Button type="submit" onClick={handlePlaceBet}>Submit</Button>
+                        </div>
+        }
     </>);
 }
